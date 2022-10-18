e17f027d2f1415f4f8b5ab85058b8c3450c01108
sergejlogacev <chizzplay@icloud.com>
2022-10-18 21:56:03 +0300

    add nav

---
+++ fragments/nav.php
@@ -0,0 +1,5 @@
+<ul class="nav">
+    <li><a class="nav-link" href="#">Проекты</a></li>
+    <li><a class="nav-link" href="#">Дипломы и конкурсы</a></li>
+</ul>
+<br>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Termolk(Логачев С.Д.)</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+
+<?php
+include 'fragments/header.php';
+include 'fragments/nav.php';
+?>
+
+<hr>
+<p><strong>Матричный калькулятор, написанный на JavaScript: <a target="_blank" href="calculateMatrix/index.html">ТЫК</a></strong></p>
+<hr>
+<p><strong>Проект-дз по Дискретной математике: <a target="_blank" href="discretnoyeMetro/index.html">КЛАЦ</a></strong></p>
+<hr>
+<p><strong>Дипломная работа школы Samsung: <a target="_blank" href="https://github.com/Termolk/right_finance">GitHub</a> и <a target="_blank" href="https://www.youtube.com/shorts/qXs9BQH-AKE">видео</a></strong></p>
+<p>Диплом: <a target="_blank" href="img/sams_ru.png">Русская версия</a> <a target="_blank" href="img/sams_en.png">Английская версия</a></p>
+<hr>
+
+</body>
+</html>
