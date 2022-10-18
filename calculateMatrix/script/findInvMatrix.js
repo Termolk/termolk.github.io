8835d2a9abb898a1dd127eba665ee90f5c8f50bb
sergejlogacev <chizzplay@icloud.com>
2022-10-18 16:22:03 +0300

    fix

--- calculateMatrix/script/findInvMatrix.js
+++ calculateMatrix/script/findInvMatrix.js
@@ -1,4 +1,11 @@
 function findInvMatrix(matrix, which) {
     whichMatrixCreate(which);
+    let determinant = math.det(matrix);
+
+    if (determinant === 0){
+        alert("Детерминант матрицы не может быть равен нулю");
+        return;
+    }
+
     createHtmlDivSolutionMatrix(which, matrix, math.inv(matrix), "Обратная матрица");
 }
